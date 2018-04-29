d170d93126d3609f613b04c1b1bc8b9d57ff8b04
Anwesh43 <anweshthecool0@gmail.com>
2018-04-29 13:30:45 +0530

    adding linetokview in Activity

--- app/src/main/java/ui/anwesome/com/kotlinlinetokview/MainActivity.kt
+++ app/src/main/java/ui/anwesome/com/kotlinlinetokview/MainActivity.kt
@@ -2,10 +2,12 @@ package ui.anwesome.com.kotlinlinetokview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import ui.anwesome.com.linetokview.LineToKView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        LineToKView.create(this)
     }
 }
